b4b2f1e0d26dcebf0d14d6d4df5a619ae347214b
fmaxime <maxime.fuzeau@ynov.com>
2023-10-22 17:14:13 +0200

    :lipstick: / improve responsive of page sites

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ func main() {
 	//handlers
 	http.HandleFunc("/", handler.Home)
 	http.HandleFunc("/acceuil", handler.Acceuil)
+	http.HandleFunc("/sites", handler.Sites)
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("front/css"))))
 	http.Handle("/fonts/", http.StripPrefix("/fonts/", http.FileServer(http.Dir("front/fonts"))))
 	http.Handle("/img/", http.StripPrefix("/img/", http.FileServer(http.Dir("front/img"))))
