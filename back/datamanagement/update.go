bffc6101eb17ecb3f1cf8e0d54b985b1e5324017
bilucas <lucas.bigot@ynov.com>
2023-10-30 16:15:14 +0100

    :sparkles: | update iban

--- back/datamanagement/update.go
+++ back/datamanagement/update.go
@@ -3,3 +3,7 @@ package datamanagement
 func Work(idUser int) {
 	AddDeleteUpdateDB("UPDATE employee SET isWorking=not isWorking WHERE idEmployee=?;", idUser)
 }
+
+func UpdateIban(idUser int, newIban string) {
+	AddDeleteUpdateDB("UPDATE employee SET IBAN=? WHERE idEmployee=?;", newIban, idUser)
+}
--- back/handler/Sites.go
+++ back/handler/Sites.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"decantez-vous/back/datamanagement"
+	"fmt"
 	"html/template"
 	"log"
 	"net/http"
@@ -45,15 +46,24 @@ func Sites(w http.ResponseWriter, r *http.Request, adress string) {
 	disconnect := r.FormValue("disconnect")
 	work := r.FormValue("work")
 	deleteEmployee := r.FormValue("deleteEmployee")
-
-	// delete supplier
-
-	if deleteEmployee != "" {
-		idDeleteSupplier, err := strconv.Atoi(deleteEmployee)
+	nIban := r.FormValue("nIban")
+	idUserSelected := r.FormValue("idUserSelected")
+	fmt.Println(nIban, idUserSelected)
+	if nIban != "" && idUserSelected != "" {
+		idUser, err := strconv.Atoi(idUserSelected)
 		if err != nil {
 			log.Fatal(err)
 		}
-		datamanagement.DeleteEmployee(idDeleteSupplier)
+		datamanagement.UpdateIban(idUser, nIban)
+	}
+	// delete employee
+	if deleteEmployee != "" {
+		idDeleteEmployee, err := strconv.Atoi(deleteEmployee)
+		if err != nil {
+			log.Fatal(err)
+		} else {
+			datamanagement.DeleteEmployee(idDeleteEmployee)
+		}
 	}
 
 	if disconnect != "" {
